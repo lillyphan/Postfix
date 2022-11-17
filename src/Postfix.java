4da07c3396a955f9356b9a1e1cd78f193d3dcbd6
lillyphan <lphan24@kentdenver.org>
2022-11-16 18:50:27 -0700

    Write main for loop of calculator containing switch case statements.

--- src/Postfix.java
+++ src/Postfix.java
@@ -1,2 +1,50 @@
+import java.util.Scanner;
+
 public class Postfix {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<>();
+        int addedInt = 0;
+        String userInput = "y";
+        boolean error = false;
+
+
+        System.out.println("enter a problem: ");
+        String exp = s.nextLine();
+
+        for (int i = 0; i < exp.length(); i++) {
+            switch (exp.charAt(i)) {
+                case '0' -> stack.push(0);
+                case '1' -> stack.push(1);
+                case '2' -> stack.push(2);
+                case '3' -> stack.push(3);
+                case '4' -> stack.push(4);
+                case '5' -> stack.push(5);
+                case '6' -> stack.push(6);
+                case '7' -> stack.push(7);
+                case '8' -> stack.push(8);
+                case '9' -> stack.push(9);
+                case ' ' -> {
+                }
+                default -> {
+                    System.out.println("in second switch" + exp.charAt(i));
+                    if (!(stack.isEmpty() || stack.top().getChild() == null)) {
+                        int x = stack.pop();
+                        int y = stack.pop();
+                        switch (exp.charAt(i)) {
+                            case '+' -> stack.push(x + y);
+                            case '-' -> stack.push(x - y);
+                            case '*' -> stack.push(x * y);
+                            case '/' -> stack.push(x / y);
+                            default -> {
+                            }
+                        }
+                    } else {
+                        error = true;
+                    }
+                }
+            }
+        }
+    }
 }
