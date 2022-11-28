7ee8f7e7f572494e4d3322a3bb9add7db3641025
lillyphan <lphan24@kentdenver.org>
2022-11-28 08:18:38 -0700

    Add comments to calculator

--- src/Postfix.java
+++ src/Postfix.java
@@ -5,38 +5,38 @@ public class Postfix {
         Scanner s = new Scanner(System.in);
         Stack<Integer> stack = new Stack<>();
         String userInput = "y";
-        do {
-            while(!stack.isEmpty()){
+        do { //do while loop to prompt the user at least once
+            while(!stack.isEmpty()){ //empty out stack before entering a new equation
                 stack.pop();
             }
-            System.out.println("enter a problem: ");
+            System.out.println("enter a problem: "); //prompt user for an equation
             String exp = s.nextLine();
-            String[] elements = exp.split(" ", -1);
-            for (String e: elements){
-                try {
+            String[] elements = exp.split(" ", -1); //split up string into it's parts (numbers + operators)
+            for (String e: elements){ //loops through all parts
+                try { //try to push element as an integer
                     stack.push(Integer.parseInt(e));
                 }
-                catch (NumberFormatException n) {
-                    if (!(stack.isEmpty() || stack.top().getChild() == null)) {
-                        int x = stack.pop();
+                catch (NumberFormatException n) { //if it doesn't work, it is not an integer
+                    if (!(stack.isEmpty() || stack.top().getChild() == null)) { //if the stack isn't empty or doesn't only have one element
+                        int x = stack.pop(); //pop top two elements
                         int y = stack.pop();
-                        switch (e) {
+                        switch (e) { //detect which operator current element is
                             case "+" -> stack.push(x + y);
                             case "-" -> stack.push(x - y);
                             case "*", "x" -> stack.push(x * y);
                             case "/" -> stack.push(x / y);
                             default -> {}
                         }
-                    } else {
+                    } else { //if the stack is empty or only has one element w/ operator, there is an error with the user input
                         System.out.println("Not enough operands/operators");
                         break;
                     }
                 }
             }
-            System.out.println("answer: " + stack.peek());
-            System.out.println("type y to solve another expression or n to exit");
+            System.out.println("answer: " + stack.peek()); //answer is at top of stack and is returned to user
+            System.out.println("type y to solve another expression or n to exit"); //update do while condition
             userInput = s.nextLine();
-        } while (userInput.equals("y"));
+        } while (userInput.equals("y")); //while user prompts yes, program will continue to run
 
     }
 
