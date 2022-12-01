39a07f3f6d0561101843758cc3a75246fa08868b
gwcorbett143 <gcorbett23@kentdenver.org>
2022-12-01 10:08:44 -0700

    TestDriver completed

--- src/testDriver.java
+++ src/testDriver.java
@@ -1,2 +1,55 @@
+import java.io.*;
+
 public class testDriver {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("tests.txt"));
+            String line = reader.readLine();
+
+            while(line != null){
+
+                Stack<Integer> stack = new Stack<>();
+                String[] elements = line.split(" ", -1);
+
+                while (!stack.isEmpty()) {
+                    stack.pop();
+                }
+
+                for (String e : elements) {
+                    try {
+                        stack.push(Integer.parseInt(e));
+                    } catch (NumberFormatException n) {
+                        if (!(stack.isEmpty() || stack.top().getChild() == null)) {
+                            int x = stack.pop();
+                            int y = stack.pop();
+                            switch (e) {
+                                case "+" -> stack.push(x + y);
+                                case "-" -> stack.push(x - y);
+                                case "*", "x" -> stack.push(x * y);
+                                case "/" -> stack.push(x / y);
+                                default -> {
+                                }
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+                }
+                System.out.println(stack.peek());
+
+                line = reader.readLine();
+
+                try(PrintWriter output = new PrintWriter(new FileWriter("log.txt",true)))
+                {
+                    output.printf("%s\r\n", stack.peek());
+                }
+                catch (Exception e) {}
+
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
 }
